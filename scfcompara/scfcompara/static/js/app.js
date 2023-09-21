eb8fcfdd77b3a93813393f3a614fc7805dea1f78
MarianoGiayetto <marianogiayetto@gmail.com>
2023-09-21 15:28:49 -0300

    otros cambios

--- scfcompara/scfcompara/static/js/app.js
+++ scfcompara/scfcompara/static/js/app.js
@@ -7,10 +7,27 @@ boton_limpiar.addEventListener('click', ()=>{
 
 const vel_puerto1 = document.querySelector(".vel_puerto1");
 const vel_puerto2 = document.querySelector(".vel_puerto2");
-console.log(vel_puerto1);
-console.log(vel_puerto2)
 
-if (vel_puerto1.innerHTML === '' & vel_puerto2.innerHTML === ''){
+function Comparar(dato1,dato2){
+    if (dato1.innerHTML === "" & dato2.innerHTML === ""){
+        dato1.className = ''
+        dato2.className = '' 
+    }else if(dato1.innerHTML === dato2.innerHTML){
+        dato1.classList.add('bg-success')
+        dato2.classList.add('bg-success') 
+        console.log(dato1)
+        console.log(dato2)       
+    }else{
+        dato1.classList.add('bg-danger')
+        dato2.classListadd('bg-danger')
+    }
+}
+
+Comparar(vel_puerto1,vel_puerto2)
+
+
+
+/* if (vel_puerto1.innerHTML === '' & vel_puerto2.innerHTML === ''){
     vel_puerto1.className = 'vel_puerto1';
     vel_puerto2.className = 'vel_puerto2';
 
@@ -23,4 +40,4 @@ if (vel_puerto1.innerHTML === '' & vel_puerto2.innerHTML === ''){
 else{
     vel_puerto1.classList.add('bg-danger')
     vel_puerto2.classList.add('bg-danger')
-}
+} */
